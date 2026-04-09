bc0257f9599e2c9dfb5014aa3c750ada7b2587b2
Abhishek Verma <abhishek.verma4607@gmail.com>
2026-04-09 09:37:19 +0530

    Add configuration settings for environment and training

---
+++ config.py
@@ -0,0 +1,16 @@
+ENV_CONFIG = {
+    "vision": False,
+    "throttle": True,
+    "gear_change": False
+}
+
+TRAINING_CONFIG = {
+    "timesteps": 50000,
+    "learning_rate": 3e-4,
+    "gamma": 0.99
+}
+
+SCENARIO = {
+    "type": "no_traffic",  # change if needed
+    "use_noise": False
+}
